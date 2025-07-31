0f09dbc2c3cb4c400213b77c2dc580ca9cf577ae
parhammohtasham <206984336+parhammohtasham@users.noreply.github.com>
2025-07-31 02:18:01 -0700

    header and hero first mobile created

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-btn");
+
+// show menu
+navToggle.addEventListener("click" , function(){
+    navMenu.classList.toggle("show-sidebar");
+});
+
+//slider
+ const nextBtnEl = document.querySelector('.next');
+  const prevBtnEl = document.querySelector('.prev');
+  const imgEls = document.querySelectorAll('.banner img');
+  let activeIndex=0;
+  imgEls[activeIndex].classList.add('show');
+  nextBtnEl.addEventListener('click',()=>{
+    imgEls[activeIndex].classList.remove('show')
+    activeIndex++
+    if(activeIndex==imgEls.length){
+        activeIndex=0
+    }
+    imgEls[activeIndex].classList.add('show')
+  })
+    prevBtnEl.addEventListener('click',()=>{
+    imgEls[activeIndex].classList.remove('show')
+    activeIndex--
+    if(activeIndex<0){
+        activeIndex=imgEls.length-1
+    }
+    imgEls[activeIndex].classList.add('show')
+  })
+  
